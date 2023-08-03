b0357cb33891a12946b8878d530439032652fa0c
MI <80389747+mi1703@users.noreply.github.com>
2023-08-03 11:23:51 +0100

    Initial commit of Spring Initializr

---
+++ src/main/java/git/mDiscovery/awssqsnotificationservice/AwsSqsNotificationServiceApplication.java
@@ -0,0 +1,13 @@
+package git.mDiscovery.awssqsnotificationservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AwsSqsNotificationServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AwsSqsNotificationServiceApplication.class, args);
+	}
+
+}
---
+++ src/test/java/git/mDiscovery/awssqsnotificationservice/AwsSqsNotificationServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package git.mDiscovery.awssqsnotificationservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AwsSqsNotificationServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
