2b1aedef0b2bb9b01608cc6aebc3c1b66f8f1a6b
MI <80389747+mi1703@users.noreply.github.com>
2023-08-23 10:14:19 +0100

    feat(AWS interaction config): add blocking sync CredentialsProvider

--- src/main/java/git/mDiscovery/awssqsnotificationservice/config/AwsConfig.java
+++ src/main/java/git/mDiscovery/awssqsnotificationservice/config/AwsConfig.java
@@ -1,7 +1,17 @@
 package git.mDiscovery.awssqsnotificationservice.config;
 
+import com.amazonaws.auth.AWSCredentialsProvider;
+import com.amazonaws.auth.BasicAWSCredentials;
+import com.amazonaws.auth.AWSStaticCredentialsProvider;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration
 public class AwsConfig {
+
+    @Bean
+    public AWSCredentialsProvider awsCredentialsProvider(@Value("") String accessKey, @Value("") String secretKey) {
+        return new AWSStaticCredentialsProvider(new BasicAWSCredentials(accessKey, secretKey));
+    }
 }
