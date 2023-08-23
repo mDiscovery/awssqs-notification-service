3322ac79014e468966bec2197b28eb2e8d114ec9
MI <80389747+mi1703@users.noreply.github.com>
2023-08-23 17:56:49 +0100

    feat(AWS interaction config): add QueueMessagingTemplate

--- src/main/java/git/mDiscovery/awssqsnotificationservice/config/AwsConfig.java
+++ src/main/java/git/mDiscovery/awssqsnotificationservice/config/AwsConfig.java
@@ -5,6 +5,7 @@ import com.amazonaws.auth.BasicAWSCredentials;
 import com.amazonaws.auth.AWSStaticCredentialsProvider;
 import com.amazonaws.services.sqs.AmazonSQSAsync;
 import com.amazonaws.services.sqs.AmazonSQSAsyncClientBuilder;
+import io.awspring.cloud.messaging.core.QueueMessagingTemplate;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
@@ -38,5 +39,9 @@ public class AwsConfig {
                 .withCredentials(awsCredentialsProvider)
                 .withRegion(region)
                 .build();
+    }
+    @Bean
+    public QueueMessagingTemplate queueMessagingTemplate(AmazonSQSAsync amazonSQSAsync) {
+        return new QueueMessagingTemplate(amazonSQSAsync);
     }
 }
