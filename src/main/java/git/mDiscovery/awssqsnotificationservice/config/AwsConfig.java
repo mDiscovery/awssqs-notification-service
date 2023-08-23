8fa807db3cc0f25921c14b8c59ee5624272b5b9c
MI <80389747+mi1703@users.noreply.github.com>
2023-08-23 17:48:21 +0100

    feat(AWS interaction config): add AmazonSQSAsync Note: bean to access Amazon SQS asynchronously

--- src/main/java/git/mDiscovery/awssqsnotificationservice/config/AwsConfig.java
+++ src/main/java/git/mDiscovery/awssqsnotificationservice/config/AwsConfig.java
@@ -3,12 +3,16 @@ package git.mDiscovery.awssqsnotificationservice.config;
 import com.amazonaws.auth.AWSCredentialsProvider;
 import com.amazonaws.auth.BasicAWSCredentials;
 import com.amazonaws.auth.AWSStaticCredentialsProvider;
+import com.amazonaws.services.sqs.AmazonSQSAsync;
+import com.amazonaws.services.sqs.AmazonSQSAsyncClientBuilder;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import software.amazon.awssdk.auth.credentials.AwsBasicCredentials;
 import software.amazon.awssdk.auth.credentials.AwsCredentialsProvider;
 import software.amazon.awssdk.auth.credentials.StaticCredentialsProvider;
+
+import static com.amazonaws.services.sqs.AmazonSQSAsyncClientBuilder.standard;
 
 @Configuration
 public class AwsConfig {
@@ -25,5 +29,14 @@ public class AwsConfig {
             @Value("spring.cloud.aws.credentials.access-key") String accessKey,
             @Value("spring.cloud.aws.credentials.secret-key") String secretKey) {
         return StaticCredentialsProvider.create(AwsBasicCredentials.create(accessKey, secretKey));
+    }
+
+    @Bean
+    public AmazonSQSAsync amazonSQSAsync(AWSCredentialsProvider awsCredentialsProvider,
+                                         @Value("") String region) {
+        return AmazonSQSAsyncClientBuilder.standard()
+                .withCredentials(awsCredentialsProvider)
+                .withRegion(region)
+                .build();
     }
 }
