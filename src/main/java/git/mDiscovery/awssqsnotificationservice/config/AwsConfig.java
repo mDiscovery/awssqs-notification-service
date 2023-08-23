f937d23286f9eaa48aa2f6b45c24ee872c0c8f02
MI <80389747+mi1703@users.noreply.github.com>
2023-08-23 10:58:44 +0100

    feat(AWS interaction config): add non-blocking async CredentialsProvider

--- src/main/java/git/mDiscovery/awssqsnotificationservice/config/AwsConfig.java
+++ src/main/java/git/mDiscovery/awssqsnotificationservice/config/AwsConfig.java
@@ -6,12 +6,24 @@ import com.amazonaws.auth.AWSStaticCredentialsProvider;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import software.amazon.awssdk.auth.credentials.AwsBasicCredentials;
+import software.amazon.awssdk.auth.credentials.AwsCredentialsProvider;
+import software.amazon.awssdk.auth.credentials.StaticCredentialsProvider;
 
 @Configuration
 public class AwsConfig {
 
     @Bean
-    public AWSCredentialsProvider awsCredentialsProvider(@Value("") String accessKey, @Value("") String secretKey) {
+    public AWSCredentialsProvider awsCredentialsProvider(
+            @Value("spring.cloud.aws.credentials.access-key") String accessKey,
+            @Value("spring.cloud.aws.credentials.secret-key") String secretKey) {
         return new AWSStaticCredentialsProvider(new BasicAWSCredentials(accessKey, secretKey));
+    }
+
+    @Bean
+    public AwsCredentialsProvider awsAsyncCredentialsProvider(
+            @Value("spring.cloud.aws.credentials.access-key") String accessKey,
+            @Value("spring.cloud.aws.credentials.secret-key") String secretKey) {
+        return StaticCredentialsProvider.create(AwsBasicCredentials.create(accessKey, secretKey));
     }
 }
