04c63de6658126a11f817431ebf6d2b8548788b3
MI <80389747+mi1703@users.noreply.github.com>
2023-08-29 20:48:52 +0100

    test: add @SqsListener receiveMessage() logger

--- src/main/java/git/mDiscovery/awssqsnotificationservice/poller/SQSNotificationsPoller.java
+++ src/main/java/git/mDiscovery/awssqsnotificationservice/poller/SQSNotificationsPoller.java
@@ -1,7 +1,12 @@
 package git.mDiscovery.awssqsnotificationservice.poller;
 
+import io.awspring.cloud.sqs.annotation.SqsListener;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Component;
-
+@Slf4j
 @Component
 public class SQSNotificationsPoller {
+    @SqsListener(value = "${sqs.notifications.queue.name}")
+    public void receiveMessage(String message) {
+        log.info("received notification: {}", message);}
 }
