a05eb42da09ceca6c6933a4b902c9f791653168c
MI <80389747+mi1703@users.noreply.github.com>
2023-08-30 18:55:43 +0100

    feat(entity to DB): inject NotificationMapper to SQSNotificationPoller

--- src/main/java/git/mDiscovery/awssqsnotificationservice/poller/SQSNotificationsPoller.java
+++ src/main/java/git/mDiscovery/awssqsnotificationservice/poller/SQSNotificationsPoller.java
@@ -1,20 +1,25 @@
 package git.mDiscovery.awssqsnotificationservice.poller;
 
 import git.mDiscovery.awssqsnotificationservice.dto.NotificationDto;
+import git.mDiscovery.awssqsnotificationservice.mapper.NotificationMapper;
 import git.mDiscovery.awssqsnotificationservice.repository.NotificationRepository;
 import io.awspring.cloud.sqs.annotation.SqsListener;
 import lombok.AllArgsConstructor;
+import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.messaging.handler.annotation.Payload;
 import org.springframework.stereotype.Component;
 @Slf4j
 @Component
-@AllArgsConstructor
+@RequiredArgsConstructor
 public class SQSNotificationsPoller {
 
     private final NotificationRepository notificationRepository;
+    private final NotificationMapper notificationMapper;
 
     @SqsListener(value = "${sqs.notifications.queue.name}")
     public void receiveMessage(@Payload NotificationDto message) {
-        log.info("received notification: {}", message);}
+        log.info("received notification: {}", message);
+
+    }
 }
