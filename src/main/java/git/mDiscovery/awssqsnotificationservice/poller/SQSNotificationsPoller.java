6b20dc7a497e115b27076efa14f130ed48bf2682
MI <80389747+mi1703@users.noreply.github.com>
2023-08-30 15:04:14 +0100

    feat: add @Payload annotation to receiveMessage() parameter

--- src/main/java/git/mDiscovery/awssqsnotificationservice/poller/SQSNotificationsPoller.java
+++ src/main/java/git/mDiscovery/awssqsnotificationservice/poller/SQSNotificationsPoller.java
@@ -3,11 +3,12 @@ package git.mDiscovery.awssqsnotificationservice.poller;
 import git.mDiscovery.awssqsnotificationservice.dto.NotificationDto;
 import io.awspring.cloud.sqs.annotation.SqsListener;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.messaging.handler.annotation.Payload;
 import org.springframework.stereotype.Component;
 @Slf4j
 @Component
 public class SQSNotificationsPoller {
     @SqsListener(value = "${sqs.notifications.queue.name}")
-    public void receiveMessage(NotificationDto message) {
+    public void receiveMessage(@Payload NotificationDto message) {
         log.info("received notification: {}", message);}
 }
