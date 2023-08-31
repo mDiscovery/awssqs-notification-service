10ab1155b1aa6b0277dfb06cf701ab8136167396
MI <80389747+mi1703@users.noreply.github.com>
2023-08-31 20:00:01 +0100

    feat(REST API): inject repository/mapper into NotificationService

--- src/main/java/git/mDiscovery/awssqsnotificationservice/service/NotificationService.java
+++ src/main/java/git/mDiscovery/awssqsnotificationservice/service/NotificationService.java
@@ -1,9 +1,17 @@
 package git.mDiscovery.awssqsnotificationservice.service;
 
+import git.mDiscovery.awssqsnotificationservice.mapper.NotificationMapper;
+import git.mDiscovery.awssqsnotificationservice.repository.NotificationRepository;
+import git.mDiscovery.awssqsnotificationservice.repository.RecipientRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
 @Service
 @RequiredArgsConstructor
 public class NotificationService {
+
+    private final NotificationRepository notificationRepository;
+    private final RecipientRepository recipientRepository;
+    private final NotificationMapper notificationMapper;
+
 }
