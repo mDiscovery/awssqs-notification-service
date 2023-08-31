637862cdf06a77b2b368dc0e22912c5446e7ff3a
MI <80389747+mi1703@users.noreply.github.com>
2023-08-31 14:46:18 +0100

    feat(link entities R2DBC): add RecipientDto field to NotificationDto

--- src/main/java/git/mDiscovery/awssqsnotificationservice/dto/NotificationDto.java
+++ src/main/java/git/mDiscovery/awssqsnotificationservice/dto/NotificationDto.java
@@ -6,5 +6,10 @@ import com.fasterxml.jackson.databind.annotation.JsonNaming;
 
 @JsonNaming(PropertyNamingStrategy.SnakeCaseStrategy.class)
 @JsonInclude(JsonInclude.Include.NON_NULL)
-public record NotificationDto(String id, String subject, String body, String recipientUid) {
+public record NotificationDto(String id,
+                              String subject,
+                              String body,
+                              String recipientUid,
+                              RecipientDto recipient) {
 }
+
