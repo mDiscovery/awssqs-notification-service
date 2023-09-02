6fbcab5c2feda4606121e53aaabad4c739be34cf
MI <80389747+mi1703@users.noreply.github.com>
2023-09-02 19:31:47 +0100

    feat(versioned REST): add @RestController NotificationControllerV2

---
+++ src/main/java/git/mDiscovery/awssqsnotificationservice/rest/NotificationControllerV2.java
@@ -0,0 +1,16 @@
+package git.mDiscovery.awssqsnotificationservice.rest;
+
+import git.mDiscovery.awssqsnotificationservice.service.NotificationService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+import reactor.core.publisher.Mono;
+
+@RestController
+@RequiredArgsConstructor
+@RequestMapping("api/v2/notifications")
+public class NotificationControllerV2 {
+    private final NotificationService notificationService;
+}
