011f56f4b69ced0b6974d0408fdd0a2873567d44
MI <80389747+mi1703@users.noreply.github.com>
2023-09-02 19:08:58 +0100

    feat(versioned REST): add @GetMapping findByUid() to NotificationControllerV1

--- src/main/java/git/mDiscovery/awssqsnotificationservice/rest/NotificationControllerV1.java
+++ src/main/java/git/mDiscovery/awssqsnotificationservice/rest/NotificationControllerV1.java
@@ -2,12 +2,20 @@ package git.mDiscovery.awssqsnotificationservice.rest;
 
 import git.mDiscovery.awssqsnotificationservice.service.NotificationService;
 import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import reactor.core.publisher.Mono;
 
 @RestController
 @RequiredArgsConstructor
 @RequestMapping("api/v1/notifications")
 public class NotificationControllerV1 {
     private final NotificationService notificationService;
+
+    @GetMapping("/{uid}")
+    public Mono<?> findByUid(String uid) {
+        return notificationService.findByUid(uid);
+    }
+
 }
