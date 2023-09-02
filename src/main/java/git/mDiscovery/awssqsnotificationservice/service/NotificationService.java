e981bed7d5876410c71a25da371d5045bc7208eb
MI <80389747+mi1703@users.noreply.github.com>
2023-09-02 13:01:22 +0100

    feat(versioned REST): add method to get Notification without Recipient

--- src/main/java/git/mDiscovery/awssqsnotificationservice/service/NotificationService.java
+++ src/main/java/git/mDiscovery/awssqsnotificationservice/service/NotificationService.java
@@ -1,10 +1,12 @@
 package git.mDiscovery.awssqsnotificationservice.service;
 
+import git.mDiscovery.awssqsnotificationservice.dto.NotificationDto;
 import git.mDiscovery.awssqsnotificationservice.mapper.NotificationMapper;
 import git.mDiscovery.awssqsnotificationservice.repository.NotificationRepository;
 import git.mDiscovery.awssqsnotificationservice.repository.RecipientRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
+import reactor.core.publisher.Mono;
 
 @Service
 @RequiredArgsConstructor
@@ -14,4 +16,8 @@ public class NotificationService {
     private final RecipientRepository recipientRepository;
     private final NotificationMapper notificationMapper;
 
+    public Mono<NotificationDto> findByUid(String uid) {
+        return notificationRepository.findById(uid)
+                .map(notificationMapper::map);
+    }
 }
