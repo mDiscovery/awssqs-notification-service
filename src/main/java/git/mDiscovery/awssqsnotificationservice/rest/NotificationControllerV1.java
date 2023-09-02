f37c7a4b8323134e32c504a22d82fb54879e2e0c
MI <80389747+mi1703@users.noreply.github.com>
2023-09-02 19:01:26 +0100

    feat(versioned REST): add @RequiredArgsConstructor to NotificationControllerV1

--- src/main/java/git/mDiscovery/awssqsnotificationservice/rest/NotificationControllerV1.java
+++ src/main/java/git/mDiscovery/awssqsnotificationservice/rest/NotificationControllerV1.java
@@ -1,10 +1,12 @@
 package git.mDiscovery.awssqsnotificationservice.rest;
 
+import git.mDiscovery.awssqsnotificationservice.service.NotificationService;
+import lombok.RequiredArgsConstructor;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
+@RequiredArgsConstructor
 @RequestMapping("api/v1/notifications")
 public class NotificationControllerV1 {
-
 }
