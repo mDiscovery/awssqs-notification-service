a3a4644f3ead6b068d69dc8a65d8b38d9fbb5d7c
MI <80389747+mi1703@users.noreply.github.com>
2023-09-02 18:43:34 +0100

    feat(versioned REST): add @RestController NotificationControllerV1

---
+++ src/main/java/git/mDiscovery/awssqsnotificationservice/rest/NotificationControllerV1.java
@@ -0,0 +1,8 @@
+package git.mDiscovery.awssqsnotificationservice.rest;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class NotificationControllerV1 {
+}
