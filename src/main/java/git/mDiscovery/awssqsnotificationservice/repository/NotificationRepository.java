7f028e47a5c033645763c1b49e856aca9172524d
MI <80389747+mi1703@users.noreply.github.com>
2023-09-05 12:31:16 +0100

    feat(versioned REST): add findAllByRecipientUid() to NotificationRepository

--- src/main/java/git/mDiscovery/awssqsnotificationservice/repository/NotificationRepository.java
+++ src/main/java/git/mDiscovery/awssqsnotificationservice/repository/NotificationRepository.java
@@ -2,6 +2,8 @@ package git.mDiscovery.awssqsnotificationservice.repository;
 
 import git.mDiscovery.awssqsnotificationservice.entity.NotificationEntity;
 import org.springframework.data.r2dbc.repository.R2dbcRepository;
+import reactor.core.publisher.Flux;
 
 public interface NotificationRepository extends R2dbcRepository<NotificationEntity, String> {
+    Flux<NotificationEntity> findAllByRecipientUid(String uid);
 }
