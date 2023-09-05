7f010aa46593dfae4bfe6c0978e67bd5f8ef4916
MI <80389747+mi1703@users.noreply.github.com>
2023-09-05 12:51:02 +0100

    feat(versioned REST): add findWithNotificationByUid() to RecipientService

--- src/main/java/git/mDiscovery/awssqsnotificationservice/service/RecipientService.java
+++ src/main/java/git/mDiscovery/awssqsnotificationservice/service/RecipientService.java
@@ -1,10 +1,16 @@
 package git.mDiscovery.awssqsnotificationservice.service;
 
+import git.mDiscovery.awssqsnotificationservice.dto.RecipientDto;
+import git.mDiscovery.awssqsnotificationservice.entity.NotificationEntity;
+import git.mDiscovery.awssqsnotificationservice.entity.RecipientEntity;
 import git.mDiscovery.awssqsnotificationservice.mapper.RecipientMapper;
 import git.mDiscovery.awssqsnotificationservice.repository.NotificationRepository;
 import git.mDiscovery.awssqsnotificationservice.repository.RecipientRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
+import reactor.core.publisher.Mono;
+
+import java.util.List;
 
 
 @Service
@@ -14,5 +20,17 @@ public class RecipientService {
     private final NotificationRepository notificationRepository;
     private final RecipientRepository recipientRepository;
     private final RecipientMapper recipientMapper;
+
+    public Mono<RecipientDto> findWithNotificationByUid(String uid) {
+        return Mono.zip(recipientRepository.findById(uid),
+                        notificationRepository.findAllByRecipientUid(uid).collectList())
+                .map(tuples -> {
+                    RecipientEntity recipient = tuples.getT1();
+                    List<NotificationEntity> notifications = tuples.getT2();
+                    recipient.setNotifications(notifications);
+                    return recipientMapper.map(recipient);
+                });
+    }
+
 
 }
