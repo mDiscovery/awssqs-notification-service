2c5e6adf3e4d1fc929f467b7cb2ada9b4cf70fc0
MI <80389747+mi1703@users.noreply.github.com>
2023-09-05 12:25:13 +0100

    feat(versioned REST): inject Repository/Mapper into RecipientService

--- src/main/java/git/mDiscovery/awssqsnotificationservice/service/RecipientService.java
+++ src/main/java/git/mDiscovery/awssqsnotificationservice/service/RecipientService.java
@@ -1,8 +1,17 @@
 package git.mDiscovery.awssqsnotificationservice.service;
 
+import git.mDiscovery.awssqsnotificationservice.mapper.RecipientMapper;
+import git.mDiscovery.awssqsnotificationservice.repository.NotificationRepository;
+import git.mDiscovery.awssqsnotificationservice.repository.RecipientRepository;
+import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
 @Service
+@RequiredArgsConstructor
 public class RecipientService {
+
+    private final NotificationRepository notificationRepository;
+    private final RecipientRepository recipientRepository;
+    private final RecipientMapper notificationMapper;
 
 }
